6a550c8ba0580bfdfb00fd54f4bc55ba5070a077
langlang.chen <langlang.chen@advantech.com.cn>
2020-11-02 17:46:29 +0800

    lib

---
+++ src/audio-player/index.js
@@ -0,0 +1,24 @@
+import AudioPlayer from './index.vue'
+
+const components = [
+  AudioPlayer
+]
+
+const install = Vue => {
+  components.forEach(Component => {
+    Vue.component(Component.name, Component)
+  })
+}
+
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export {
+  AudioPlayer
+}
+
+export default {
+  install
+}
